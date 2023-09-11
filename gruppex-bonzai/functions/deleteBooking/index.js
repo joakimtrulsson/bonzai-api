12ff3821a6e4530f5bcc8a2698f571f62735231e
HerrPablito <107303243+HerrPablito@users.noreply.github.com>
2023-09-11 15:22:45 +0200

    update delete
    First part of update delete

--- gruppex-bonzai/functions/deleteBooking/index.js
+++ gruppex-bonzai/functions/deleteBooking/index.js
@@ -3,13 +3,19 @@ const { db } = require("../../services/db");
 
 
 exports.handler = async (event, context) => {
+    const { bookingId } = event.pathParameters;
 
     try {
-        
+        await db.delete({
+            TableName: "bookingsDb",
+            Key: {
+                bookingId: bookingId,
+            },
+        }).promise();
 
         return sendResponse(200, { success: true } );
     } catch (error) {
      console.log(error);
-        return sendError(500, error);
+        return sendError(500, { success: false, message: "could not delete booking"});
     }
 }
