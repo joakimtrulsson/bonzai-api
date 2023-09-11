0df6fe7c757d8f27dc637c8c1a541723e23128bb
HerrPablito <107303243+HerrPablito@users.noreply.github.com>
2023-09-11 15:28:40 +0200

    Update get att bookings
    update get all bookings

--- gruppex-bonzai/functions/getAllBookings/index.js
+++ gruppex-bonzai/functions/getAllBookings/index.js
@@ -5,11 +5,17 @@ const { db } = require("../../services/db");
 exports.handler = async (event, context) => {
 
     try {
-        
+        const { Items: bookings } = await db.scan({
+            TableName: "bookingsDb",
+            FilterExpression: "attribute_exists(#DYNOBASE_bookingId)",
+            ExpressionAttributeNames: {
+                "#DYNOBASE_bookingId": "bookingId",
+            },
+        }).promise();
 
-        return sendResponse(200, { success: true } );
+        return sendResponse(200, { Bookings: bookings } );
     } catch (error) {
      console.log(error);
-        return sendError(500, error);
+        return sendError(500, { success: false, message: "could not get bookings" });
     }
 }
