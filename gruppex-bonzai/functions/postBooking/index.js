3302572bcc2d00c82fe75753bbb86f3978c4cfd3
Joakim Trulsson <joakim.trulsson@icloud.com>
2023-09-11 12:36:45 +0200

    env file

--- gruppex-bonzai/functions/postBooking/index.js
+++ gruppex-bonzai/functions/postBooking/index.js
@@ -1,15 +1,32 @@
-const { sendResponse, sendError } = require("../../responses/index");
-const { db } = require("../../services/db");
-
+const { sendResponse, sendError } = require('../../responses/index');
+const { db } = require('../../services/db');
 
 exports.handler = async (event, context) => {
+  try {
+    const requestData = JSON.parse(event.body);
 
-    try {
-        
+    // const bookingId = nanoId()
 
-        return sendResponse(200, { success: true } );
-    } catch (error) {
-     console.log(error);
-        return sendError(500, error);
-    }
-}
+    function calculateTotalCost(price, days) {}
+
+    const params = {
+      TableName: 'bookingDb',
+      Item: {
+        bookingId: bookingId,
+        guestName: requestData.guestName,
+        checkInDate: requestData.checkInDate,
+        checkOutDate: requestData.checkOutDate,
+        roomIds: requestData.roomIds,
+        totalCost: calculateTotalCost(price, days),
+        booked: true,
+      },
+    };
+
+    await db.put(params).promise();
+
+    return sendResponse(200, { success: true, message: 'Bokning har skapats. Returnera ID.' });
+  } catch (error) {
+    console.log(error);
+    return sendError(500, error);
+  }
+};
